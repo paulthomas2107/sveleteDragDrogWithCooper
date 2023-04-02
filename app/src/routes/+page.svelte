dd7b380345c30bd376ac57973284e0c9ddb139fa
Paul Thomas <paulthomas2107@gmail.com>
2023-04-02 12:35:36 +0100

    More

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
+	import { flip } from 'svelte/animate';
 	import { dndzone } from 'svelte-dnd-action';
+
+	const flipDurationMs = 100;
 
 	interface ListItem {
 		id: number;
 		title: string;
 		description: string;
+		tags: string[];
 	}
 
 	let items: ListItem[] = [
 		{
 			id: 1,
 			title: 'Blog Post 1',
-			description: 'Blog Post 1 Description'
+			description: 'Blog Post 1 Description',
+			tags: ['CooperCodes']
 		},
 		{
 			id: 2,
 			title: 'Blog Post 2',
-			description: 'Blog Post 2 Description'
+			description: 'Blog Post 2 Description',
+			tags: ['Development']
 		}
 	];
 
@@ -32,12 +38,24 @@
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<section use:dndzone={{ items: items }} on:consider={handleConsider} on:finalize={handleFinalize}>
+	<section
+		use:dndzone={{ items: items, flipDurationMs: flipDurationMs, dropTargetStyle: {} }}
+		on:consider={handleConsider}
+		on:finalize={handleFinalize}
+	>
 		{#each items as item (item.id)}
-			<div class="card card-hover w-96 my-4">
+			<div class="card card-hover w-96 my-4" animate:flip={{ duration: flipDurationMs }}>
 				<header class="card-header">
 					<h4>{item.title}</h4>
 				</header>
+				<section class="p-4">
+					{item.description}
+				</section>
+				<footer class="card-footer inline-block">
+					{#each item.tags as tag}
+						<span class="chip variant-filled-secondary">{tag}</span>
+					{/each}
+				</footer>
 			</div>
 		{/each}
 	</section>
